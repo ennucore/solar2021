ac5f16a0a14185cb6b1b3b8cc01c06b65df90f6e
Alex <mametevalex@gmail.com>
2021-11-14 12:31:11 +0300

    Нормальный расчет силы действующую на тело

--- solar_model.py
+++ solar_model.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 # license: GPLv3
+from solar_objects import SpaceObject
+import numpy as np
+from numpy import sin, cos, atan
 
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
@@ -15,13 +18,18 @@ def calculate_force(body, space_objects):
     **space_objects** — список объектов, которые воздействуют на тело.
     """
 
-    body.Fx = body.Fy = 0
+    force = np.array(0, 0)
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
-        pass  # FIXME: Взаимодействие объектов
+        r2 = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2)
+        r2 = max(r2, body.R ** 2)  # FIXME: обработка аномалий при прохождении одного тела сквозь другое
+        abs_df = gravitational_constant * body.m * obj.m / r2  # Модуль силы именно этого взаимодействия
+        angle = atan((obj.y - body.y) / (obj.x - body.x))
+        df = abs_df * np.array(cos(angle), sin(angle))
+        force += df
+    body.update_params(Fx=force[0], Fy=force[1])
+
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
@@ -31,11 +39,11 @@ def move_space_object(body, dt):
     **body** — тело, которое нужно переместить.
     """
     old = body.x  # FIXME: Вывести формулы для ускорения, скоростей и координат
-    ax = body.Fx/body.m
+    ax = body.Fx / body.m
     body.x += 24
-    ay = body.Fy*body.m
+    ay = body.Fy * body.m
     body.y = 42
-    body.Vy += 4*dt
+    body.Vy += 4 * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
